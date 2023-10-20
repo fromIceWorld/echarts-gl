c8d5e8d435ff51e2c6ccfa69a44cbdeb6136267a
Cui Bingbing <752856108@qq.com>
2023-10-20 10:35:58 +0800

    feat:清理文件

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    ngZone: 'noop',
-  })
-  .catch((err) => console.error(err));
